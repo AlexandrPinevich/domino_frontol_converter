26ba141b7519a6d339fd52d5b0a0005bef2b756a
Alex Pinevich @rtfmpal <4zakaza@gmail.com>
2025-04-08 15:38:34 +0300

    extractor script added

--- scripts/frontol_converter_run.bat
+++ scripts/frontol_converter_run.bat
@@ -1,21 +1,22 @@
 @echo off
-:: Указываем путь к Python
+rem Указываем путь к Python
+rem Get-Command python
 set "PYTHON_PATH=C:\Python313\python.exe"
 
-:: Указываем путь к скрипту
+rem Указываем путь к скрипту
 set "SCRIPT_PATH=C:\Users\A.Pinevich\YandexDisk\domino_frontol_converter\src\domino_frontol_for_kas_converter.py"
 
-:: Извлекаем день, месяц и год из текущей даты
+rem Извлекаем день, месяц и год из текущей даты
 set day=%date:~0,2%
 set month=%date:~3,2%
 set year=%date:~6,4%
 
-:: Формируем дату в формате ГГГГ-ММ-ДД
+rem Формируем дату в формате ГГГГ-ММ-ДД
 set LOG_DATE=%year%-%month%-%day%
 
 set "LOG_FILE=C:\Users\A.Pinevich\YandexDisk\domino_frontol_converter\data\FOR_KAS\2_CONVERT_LOG\execution_%LOG_DATE%.log"
 
-:: Проверяем, существует ли Python
+rem Проверяем, существует ли Python
 if exist "%PYTHON_PATH%" (
     echo [%date% %time%] Запуск скрипта... >> "%LOG_FILE%"
     "%PYTHON_PATH%" "%SCRIPT_PATH%" >> "%LOG_FILE%" 2>&1
---
+++ scripts/frontol_extractor.bat
@@ -0,0 +1,63 @@
+@echo off
+setlocal enabledelayedexpansion
+
+rem ========== SETTINGS ==========
+set "source_folder=C:\domino_frontol_converter\data\FROM_KAS"
+set "destination_folder=\\server\domino\MAIL\FROM_KAS"
+set "log_folder=\\server\domino\MAIL\FROM_KAS\Frontol_extract_log"
+set "kas_number=kas_2"
+set "log_file=%log_folder%\%kas_number%.log"
+
+rem ========== PATH VALIDATION ==========
+if not exist "%source_folder%\" (
+    echo [FAIL] Source folder missing: %source_folder%
+    pause & exit /b 1
+)
+
+if not exist "%destination_folder%\" (
+    echo [FAIL] Destination folder missing: %destination_folder%
+    pause & exit /b 2
+)
+
+if not exist "%log_folder%\" (
+    echo [FAIL] Log folder missing: %log_folder%
+    pause & exit /b 3
+)
+
+rem === MAIN OPERATION ===
+
+echo [INFO] Starting script 
+echo [INFO] Starting script >> "%log_file%"
+
+rem --- File count ---
+set "file_count=0"
+for /f %%i in ('dir /b /a-d "%source_folder%" 2^>nul ^| find /c /v ""') do set file_count=%%i
+echo [%date% %time%] [DEBUG] file_count="!file_count!" 
+echo [%date% %time%] [DEBUG] file_count="!file_count!" >> "%log_file%"
+
+rem --- File move ---
+if !file_count! equ 0 (
+    echo [%date% %time%] [WARN] No files to process in %source_folder%
+    echo [%date% %time%] [WARN] No files to process in %source_folder%  >> "%log_file%"      
+) else (
+    echo [%date% %time%] [INFO] moving !file_count! files from %source_folder% to %destination_folder%
+    echo [%date% %time%] [INFO] moving !file_count! files from %source_folder% to %destination_folder% >> "%log_file%"        
+    robocopy "%source_folder%" "%destination_folder%" *.* /MOV /NJH /NJS /NDL /NC /NS /NP
+    set "robocopy_errorlevel=!errorlevel!"
+    if !robocopy_errorlevel! geq 8 (
+        echo [%date% %time%] [ERROR] Critical Robocopy error (code: !robocopy_errorlevel!^)
+        echo [%date% %time%] [ERROR] Critical Robocopy error (code: !robocopy_errorlevel!^) >> "%log_file%"            
+    ) 
+    if !robocopy_errorlevel! leq 7 (
+        echo [%date% %time%] [OK] Success Robocopy error level (code: !robocopy_errorlevel!^)
+        echo [%date% %time%] [OK] Success Robocopy error level (code: !robocopy_errorlevel!^) >> "%log_file%"            
+    )
+)
+
+rem ========== FINAL OUTPUT ==========
+echo [OK] Script finished
+echo [OK] Script finished >> "%log_file%"
+rem pause
+timeout 3 >nul
+exit /b 0
+
